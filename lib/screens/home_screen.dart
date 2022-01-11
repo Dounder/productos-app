4926898a4e113021efbc30291d6341875cd1e00a
dcode <dr.glasdou@gmail.com>
2022-01-10 19:04:26 -0600

    done: app ui basic

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Productos')),
+      body: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        itemCount: 10,
+        itemBuilder: (_, int i) => GestureDetector(
+          child: const ProductCard(),
+          onTap: () => Navigator.pushNamed(context, 'product'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
